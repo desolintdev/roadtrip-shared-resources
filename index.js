df32acd49002d6848eaf1e18bc7353f977705580
syedali042 <salizahid75@gmail.com>
2024-07-31 16:16:04 +0500

    Chore: customers model deleted.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const ProductsModel = require(`./ProductsModel`);
 const DraftsModel = require(`./DraftsModel`);
-const CustomersModel = require(`./CustomersModel`);
 
 module.exports = {
   ProductsModel,
   DraftsModel,
-  CustomersModel,
 };
