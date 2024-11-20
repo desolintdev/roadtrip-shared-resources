38f83fa935a5dd4787f7d8174d27da32bd1da8ac
syedali042 <salizahid75@gmail.com>
2024-11-20 12:18:08 +0500

    Feat: display name field added in cities.

--- CitiesModel.js
+++ CitiesModel.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const displayNameSchema = new Schema({
+  _id: false,
+  en: {
+    type: String,
+  },
+  nl: {
+    type: String,
+  },
+});
+
 const citiesSchema = new Schema(
   {
     name: {
@@ -23,6 +33,9 @@ const citiesSchema = new Schema(
     },
     remarks: {
       type: String,
+    },
+    displayName: {
+      type: displayNameSchema,
     },
   },
   {timestamps: true, toObject: {virtuals: true}, toJSON: {virtuals: true}}
