7968d0c6f90534488c903f1ccaa83326ae22fb6b
saliksajjad08@gmail.com <salizahid75@gmail.com>
2025-01-28 19:22:27 +0500

    Fix: code rabbit ai comments.

--- GuidesModel.js
+++ GuidesModel.js
@@ -6,10 +6,16 @@ const stopsSchema = new Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
+    minlength: [3, 'Name must be at least 5 characters long'],
+    maxlength: [100, 'Name cannot exceed 200 characters'],
   },
   description: {
     type: String,
     required: true,
+    trim: true,
+    minlength: [5, 'Description must be at least 5 characters long'],
+    maxlength: [1000, 'Description cannot exceed 200 characters'],
   },
   images: {
     type: [String],
@@ -21,14 +27,26 @@ const guidesSchema = new Schema(
     title: {
       type: String,
       required: true,
+      trim: true,
+      minlength: [5, 'Title must be at least 5 characters long'],
+      maxlength: [100, 'Title cannot exceed 200 characters'],
     },
     description: {
       type: String,
       required: true,
+      trim: true,
+      minlength: [20, 'Description must be at least 20 characters long'],
+      maxlength: [1000, 'Description cannot exceed 10000 characters'],
     },
     stops: {
       type: [stopsSchema],
       required: true,
+      validate: {
+        validator: function (v) {
+          return v && v.length > 0;
+        },
+        message: 'A guide must have at least one stop',
+      },
     },
   },
   {timestamps: true, toObject: {virtuals: true}, toJSON: {virtuals: true}}
